00652a92e0411aac2b06b0ea2a1b2bde80559c21
SamZ Ota <sam.otb@hotmail.ca>
2018-09-03 03:33:02 -0400

    booking form

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3333
 const path = require('path')
+var os = require("os");
+var hostname = os.hostname();
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')))
 app.use(express.static(__dirname + '/public'))
-app.listen(port, () => console.log('app listening on port '+port+'!'))
+app.listen(port, () => console.log('app listening on port '+port+'! and address ' +hostname))
--- public/js/app.js
+++ public/js/app.js
@@ -126,5 +126,35 @@
 	$(window).ready(function(){
 
 	});
+	//FORM
+	$(document).ready(function(){
+		$('.calendar-call').on('click', function(){
+			$('#overlay').fadeIn(300); 
+			$('.calendar').fadeIn(300); 
+			let clickedbutton = $("input",$(this).parent()).attr('id');
+			$('.dates').data('type',clickedbutton);
+		});
+		
+		$('table').on('click', function(event){
+		  let that=$(event.target);
+		   if(that.is('td') && !that.hasClass('notCurMonth') && !that.hasClass('holiday') && !that.hasClass('curDay')){
+			   $('td.curDay').toggleClass('curDay');
+			   that.toggleClass('curDay');
+		   }
+	   }); 
+	   
+	   $('#add_event').on('click', function(){
+		   let value= $('td.curDay').html();
+		   $('#overlay').fadeOut(300);
+			$('.calendar').fadeOut(300);
+			let id=($('.dates').data()).type;
+			$('#' + id).val(value+" May, 2014");
+	   }); 
+	   
+	   $('#search').on('click', function(e){
+		   $('.booking').addClass('is-sent');
+		   e.preventDefault();
+	   });
+	   });	
 
 })(jQuery, document, window);
